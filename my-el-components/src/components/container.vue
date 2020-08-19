3ad3b5f9b631d06c1de00520e7725144f7b3a7d9
zzhweb1998 <995061388@qq.com>
2020-08-19 12:01:03 +0800

    页面布局1

---
+++ my-el-components/src/components/container.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-container>
+    <el-aside width="220px">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span>导航二</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-menu"></i>
+            <span>导航三</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="3-1">选项1</el-menu-item>
+            <el-menu-item index="3-2">选项2</el-menu-item>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<style lang="less" scoped>
+.el-container {
+  height: calc(100% - 60px);
+  .el-aside {
+    margin: 20px 0;
+    border-right: 1px solid #ccc;
+  }
+  .el-main {
+  }
+}
+</style>
