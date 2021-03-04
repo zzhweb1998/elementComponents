f56ea94176213c6c31a5322b78b119e24f506e5b
周志航 <995061388@qq.com>
2021-03-04 22:31:55 +0800

    调整导航栏

--- my-element-components/src/views/container.vue
+++ my-element-components/src/views/container.vue
@@ -1,68 +1,38 @@
 <template>
   <el-container>
     <el-aside>
-      <el-menu
-        :default-active="$route.path"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        router
-        active-text-color="#409EFF"
-      >
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span>Form组件</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="/form/dynamicContainer">动态</el-menu-item>
-            <el-menu-item index="/form/recursionContainer">递归</el-menu-item>
-            <el-menu-item index="/form/enterForm">回车跳转</el-menu-item>
-            <el-menu-item index="/form/verificationContainer">验证码</el-menu-item>
-            <el-menu-item index="/form/imageContainer">图片</el-menu-item>
-            <el-menu-item index="/form/goodsContainer">商品</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span>Table组件</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="/table/customContainer">自定义</el-menu-item>
-            <el-menu-item index="/table/CommonlyContainer">常用Table</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span>Layout布局</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="/layout/waterfallContainer">瀑布流</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span>Api接口</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="/test/test1">测试</el-menu-item>
-            <el-menu-item index="/test/webSocket">聊天</el-menu-item>
-            <el-menu-item index="/test/login">登录</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="5">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span>其它组件</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="/timer/timer">时钟</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+        <el-radio-button :label="false">练习</el-radio-button>
+        <el-radio-button :label="true">组件库</el-radio-button>
+      </el-radio-group>
+      <div class="menuBox" v-show="!isCollapse">
+        <el-menu v-for="(itm,index) in demo" :key="index" :default-active="$route.path" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+          router active-text-color="#409EFF">
+          <el-submenu :index="index">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>{{itm.title}}</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item v-for="(itm2,index2) in itm.children" :key="index2" :index="itm2.url">{{itm2.name}}</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </div>
+      <div class="menuBox" v-show="isCollapse">
+        <el-menu v-for="(itm,index) in components" :key="index" :default-active="$route.path" class="el-menu-vertical-demo" @open="handleOpen"
+          @close="handleClose" router active-text-color="#409EFF">
+          <el-submenu :index="index">
+            <template slot="title">
+              <i class="el-icon-menu"></i>
+              <span>{{itm.title}}</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item v-for="(itm2,index2) in itm.children" :key="index2" :index="itm2.url">{{itm2.name}}</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </div>
     </el-aside>
     <el-main>
       <router-view></router-view>
@@ -72,12 +42,100 @@
 
 <script>
 export default {
-  data() {
-    return {};
+  data () {
+    return {
+      isCollapse: false,
+      demo: [
+        {
+          title: 'Form组件',
+          children: [
+            {
+              name: '动态',
+              url: '/form/dynamicContainer'
+            },
+            {
+              name: '递归',
+              url: '/form/recursionContainer'
+            },
+            {
+              name: '回车跳转',
+              url: '/form/enterForm'
+            },
+            {
+              name: '验证码',
+              url: '/form/verificationContainer'
+            },
+            {
+              name: '图片',
+              url: '/form/imageContainer'
+            },
+            {
+              name: '商品',
+              url: '/form/goodsContainer'
+            }
+          ]
+        },
+        {
+          title: 'Table组件',
+          children: [
+            {
+              name: '自定义',
+              url: '/table/customContainer'
+            },
+            {
+              name: '常用Table',
+              url: '/table/CommonlyContainer'
+            }
+          ]
+        },
+        {
+          title: 'Layout布局',
+          children: [
+            {
+              name: '瀑布流',
+              url: '/layout/waterfallContainer'
+            }
+          ]
+        },
+        {
+          title: 'Api接口',
+          children: [
+            {
+              name: '测试',
+              url: '/test/test1'
+            },
+            {
+              name: '聊天',
+              url: '/test/webSocket'
+            }
+          ]
+        },
+        {
+          title: '其它组件',
+          children: [
+            {
+              name: '时钟',
+              url: '/timer/timer'
+            },
+          ]
+        },
+      ],
+      components: [
+        {
+          title: '测试',
+          children: [
+            {
+              name: '测试',
+              url: ''
+            },
+          ]
+        },
+      ]
+    };
   },
   methods: {
-    handleOpen() {},
-    handleClose() {},
+    handleOpen () { },
+    handleClose () { },
   },
 };
 </script>
@@ -89,6 +147,9 @@ export default {
     width: 220px !important;
     margin: 20px 0;
     border-right: 1px solid #ccc;
+    .el-radio-group {
+      margin-left: 30px;
+    }
   }
   .el-main {
     height: 100%;
